a1b82d428fae95e7ec8649050679c9089871a4eb
francis85a <franperezcid85@gmail.com>
2026-03-13 14:14:30 +0100

    test(Player test): Definir setup de player

--- src/test/java/edu/teamrocket/golfstrokes/PlayerTest.java
+++ src/test/java/edu/teamrocket/golfstrokes/PlayerTest.java
@@ -2,21 +2,25 @@ package edu.teamrocket.golfstrokes;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+    private Player playerMJ;
+
+    @BeforeEach
+    void setUp() {
+        playerMJ = new Player("MJ", (byte) 0);
+    }
 
     @Test
     void constructorCreatesPlayerObject() {
-        Player playerMJ = new Player("MJ", (byte) 0);
         assertNotNull(playerMJ);
     }
 
     @Test
     void constructorGetsNameAndStrokes() {
-        Player playerMJ = new Player("MJ", (byte) 0);
         assertEquals("MJ", playerMJ.getInitials());
-
         assertEquals((byte) 0, playerMJ.getHandicap());
     }
 
